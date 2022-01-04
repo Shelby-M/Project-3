ad18f252744113179334278d273a39242c60d42e
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-04 14:50:08 -0500

    remove uneeded files

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,30 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import'semantic-ui-css/semantic.min.css';
+// import'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import Navbar from './Navbar';
-import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
-import Footer from './Footer';
-import Post from './Post';
+// import Navbar from './Navbar';
+// import Home from './Home';
+// import Login from './Login';
+// import Signup from './Signup';
+// import Footer from './Footer';
+// import Post from './Post';
 
 
 function App() {
   return (
     <main className='App'>
-      <Router>
+      <div>Hi!</div>
+      {/* <Router>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/post" component={Post} />
-        </Router>
-    <Navbar />
-    <Home />
-    <Footer />
-    <Post />
+        </Router> */}
+    {/* <Navbar /> */}
+   
   </main>
   );
 }
