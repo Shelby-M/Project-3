cbe0e05661272f6eec077b3b21bd46501875a662
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-04 14:37:53 -0500

    app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import Footer from './Footer';
+import Post from './Post';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/post" component={Post} />
+        </Router>
+    <Navbar />
+    <Home />
+    <Footer />
+    <Post />
+  </main>
   );
 }
 
 export default App;
+
+// export default function App() {
+//   return (
+//     <main>
+//       <Navbar />
+//       <About />
+//       <Home />
+//       <Footer />
+//       <Post />
+//     </main>
+//   );
+// }
+
