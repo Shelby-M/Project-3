a8d27f5f04c6861dca62ce7c2e9e295e14591149
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-04 21:50:46 -0500

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 
 import React from 'react';
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -27,34 +26,10 @@ function App() {
     {/* <Navbar /> */}
    
   </main>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> Stashed changes
   );
 }
 
 export default App;
-<<<<<<< Updated upstream
 
 // export default function App() {
 //   return (
@@ -68,5 +43,3 @@ export default App;
 //   );
 // }
 
-=======
->>>>>>> Stashed changes
