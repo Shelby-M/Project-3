ddc058dcfd275063be7ae1202f2c237b183de007
bdiaz28 <brittanymdiaz@yahoo.com>
2022-01-04 19:38:09 -0500

    wrong branch

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,30 +1,30 @@
-// import React from 'react';
+import React from 'react';
 
 
-// function Navbar(props) {
-//   const tabs = ['Home', 'Login', 'Signup', 'Post'];
-//   return (
-//     <nav>
-//     <div>
-//     <ul>
-//       {tabs.map(tab => (
-//         <li className="nav-link" key={tab}>
-//           <a
-//             href={'#' + tab.toLowerCase()}
+function Navbar(props) {
+  const tabs = ['Home', 'Login', 'Signup', 'Post'];
+  return (
+    <nav>
+    <div>
+    <ul>
+      {tabs.map(tab => (
+        <li className="nav-link" key={tab}>
+          <a
+            href={'#' + tab.toLowerCase()}
       
-//             onClick={() => props.handlePageChange(tab)}
-//             className={
-//               props.currentPage === tab ? 'nav-link active' : 'nav-link'
-//             }
-//           >
-//             {tab}
-//           </a>
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-// </nav>
-//   );
-// }
+            onClick={() => props.handlePageChange(tab)}
+            className={
+              props.currentPage === tab ? 'nav-link active' : 'nav-link'
+            }
+          >
+            {tab}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+</nav>
+  );
+}
 
-// export default Navbar;
+export default Navbar;
